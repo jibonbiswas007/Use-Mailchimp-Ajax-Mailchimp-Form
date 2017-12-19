bcd8ecceabfc702776b51c15f356738f132fd1fe
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2017-12-19 23:06:32 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+    	$('.mail-chimp-form').ajaxChimp({
+    		url: 'https://grameenphone.us17.list-manage.com/subscribe/post?u=8cce06ad79650bcae41115969&amp;id=98291482f1'
+		});
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
